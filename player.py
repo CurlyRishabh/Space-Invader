7f2583c4417b94f63e6aecd0ccee29b722df0018
CurlyRishabh <rissag940@gmail.com>
2024-04-09 17:00:51 +0530

    laser shoot print shoo when pressed space every 600ms

--- player.py
+++ player.py
@@ -13,8 +13,14 @@ class Player(pygame.sprite.Sprite):
         '''
         self.image = pygame.image.load('./graphics/player.png').convert_alpha()
         self.rect = self.image.get_rect(midbottom=pos)
+        # varibles for player movements
         self.speed = speed
         self.max_constraint = constraint
+
+        # variables for laser shoot
+        self.ready = True
+        self.laser_time = 0
+        self.laser_cooldown = 600
 
     def get_input(self):
         keys = pygame.key.get_pressed()
@@ -22,6 +28,10 @@ class Player(pygame.sprite.Sprite):
             self.rect.x += self.speed
         elif keys[pygame.K_LEFT]:
             self.rect.x -= self.speed
+        elif keys[pygame.K_SPACE] and self.ready:
+            self.shoot_laser()
+            self.ready = False
+            self.laser_time = pygame.time.get_ticks()
 
     def constraint(self):
         if self.rect.left <= 0:
@@ -29,6 +39,16 @@ class Player(pygame.sprite.Sprite):
         if self.rect.right >= self.max_constraint:
             self.rect.right = self.max_constraint
 
+    def recharge(self):
+        if not self.ready:
+            current_time = pygame.time.get_ticks()
+            if current_time - self.laser_time >= self.laser_cooldown:
+                self.ready = True
+
+    def shoot_laser(self):
+        print("shoo")
+
     def update(self):
         self.get_input()
         self.constraint()
+        self.recharge()
