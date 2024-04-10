b1e107ed9fca9a22194976a6724459ab54190a2e
CurlyRishabh <rissag940@gmail.com>
2024-04-10 11:05:54 +0530

    created multiple obstacle

--- main.py
+++ main.py
@@ -18,18 +18,32 @@ class Game:
         self.shape = obstacle.shape
         self.block_size = 6
         self.blocks = pygame.sprite.Group()
-        self.create_obstacle(40, 480)
+        self.obstacle_number = 4
+        self.obstacle_x_positions = [
+            float((num + (1/self.obstacle_number))
+                  *
+                  (SCREEN_WIDTH / (self.obstacle_number)))
+            for num in range(self.obstacle_number)
+        ]
+        self.create_multiple_obstacle(x_start=0,
+                                      y_start=480,
+                                      offset=self.obstacle_x_positions
+                                      )
 
-    def create_obstacle(self, x_start, y_start):
+    def create_obstacle(self, x_start, y_start, offset_x):
         for row_index, row in enumerate(self.shape):
             for col_index, col in enumerate(row):
                 if col == 'x':
 
-                    x = x_start + col_index * self.block_size
+                    x = x_start + col_index * self.block_size + offset_x
                     y = y_start + row_index * self.block_size
                     block = obstacle.Block(self.block_size,
                                            (241, 79, 80), x, y)
                     self.blocks.add(block)
+
+    def create_multiple_obstacle(self, x_start, y_start, offset):
+        for offset_x in offset:
+            self.create_obstacle(x_start, y_start, offset_x)
 
     def run(self):
         'update/draw sprite groups'
